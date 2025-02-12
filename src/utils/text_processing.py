2d21e285afc9d5303d1f265efcf4f6dc9ee57b5b
safaa-suliman <120365359+safaa-suliman@users.noreply.github.com>
2025-02-13 01:15:30 +0200

    Update text_processing.py

--- src/utils/text_processing.py
+++ src/utils/text_processing.py
@@ -9,7 +9,6 @@ nltk_data_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__
 nltk.data.path.append(nltk_data_path)
 required_nltk_resources = ['punkt', 'stopwords']
 
-
 for resource in required_nltk_resources:
     try:
         nltk.data.find(f'{resource}')
@@ -20,4 +19,4 @@ def preprocess_text(text, language='english'):
     stop_words = set(stopwords.words(language))
     linking_words = set(['and', 'or', 'but', 'so', 'because', 'however', 'therefore', 'moreover', 'thus', 'hence'])
     words = word_tokenize(re.sub(r'\W+', ' ', text.lower()))  # Tokenize and clean text
-    return [word for word in words if word.isalnum() and word not in stop_words and word not in linking_words]
+    return [word for word in words if word is not None and word.isalnum() and word not in stop_words and word not in linking_words]
